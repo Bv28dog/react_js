6a52448aa3fc77e29b5c9b99287d1b596a18bffd
ostap <bv28dog@gmail.com>
2023-12-08 21:54:21 +0200

    formik done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { Table } from "./components/componentTable/Table";
+import { Formik } from "formik";
+import { FormLog } from "./components/componentsForm/FormLog";
 
 const App = () => {
   return (
     <>
-      <Table />
+      <Formik
+        initialValues={{ firstName: "", lastName: "", city: "" }}
+        onSubmit={(values, formikBag) => {
+          const errors = {};
+          if (!values.firstName) {
+            errors.firstName = "First Name is required";
+          }
+          if (!values.lastName) {
+            errors.lastName = "Last Name is required";
+          }
+          if (!values.city) {
+            errors.city = "City is required";
+          }
+          if (Object.keys(errors).length > 0) {
+            formikBag.setErrors(errors);
+          } else {
+            formikBag.setSubmitting(true);
+            formikBag.resetForm();
+          }
+        }}
+      >
+        {(props) => {
+          console.log(props);
+          return <FormLog />;
+        }}
+      </Formik>
     </>
   );
 };
