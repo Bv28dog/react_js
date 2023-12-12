daed075a389ce9a6716a5b5cb9c96dd1ce90e056
ostap <bv28dog@gmail.com>
2023-12-12 19:53:37 +0200

    hookForm done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
 import React from "react";
-import { Formik } from "formik";
-import { FormLog } from "./components/componentsForm/FormLog";
+import { HookFoorm } from "./components/componentHookForm/HookFrom";
 
 const App = () => {
   return (
     <>
-      <Formik
-        initialValues={{ firstName: "", lastName: "", city: "" }}
-        onSubmit={(values, formikBag) => {
-          const errors = {};
-          if (!values.firstName) {
-            errors.firstName = "First Name is required";
-          }
-          if (!values.lastName) {
-            errors.lastName = "Last Name is required";
-          }
-          if (!values.city) {
-            errors.city = "City is required";
-          }
-          if (Object.keys(errors).length > 0) {
-            formikBag.setErrors(errors);
-          } else {
-            formikBag.setSubmitting(true);
-            formikBag.resetForm();
-          }
-        }}
-      >
-        {(props) => {
-          console.log(props);
-          return <FormLog />;
-        }}
-      </Formik>
+      <HookFoorm />
     </>
   );
 };
